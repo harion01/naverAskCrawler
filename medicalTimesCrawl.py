681f28428e24852fa692de1070ec860794bd253c
harion01 <harion01@gmail.com>
2023-03-26 22:24:19 +0900

    modify medigate

--- medicalTimesCrawl.py
+++ medicalTimesCrawl.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from bs4 import BeautifulSoup
 from datetime import datetime
 import time
 from selenium.webdriver.common.by import By
@@ -15,26 +14,28 @@ def main():
     date = datetime.now().strftime("%Y-%m-%d_%H_%M_%S")
     filename = f"result/mediGate/url_list_medigate_IssueList_{date}.txt"
 
-    with open(filename, 'w') as fname:
+    with open(filename, 'w', encoding='utf-8') as fname:
         page_url = []
 
-        for pageIndex in range(3) :
+        for pageIndex in range(100) :
             time.sleep(uniform(0.01, 1.0))
             ##medigate issue list
             url = f'https://www.medicaltimes.com/Main/News/List_Opinion.html?page={pageIndex}&MainCate=7&SubCate=70&TargetDate=&keyword='
-            driver.get(url)
+            try:
+                driver.get(url)
+            except :
+                break
 
+            html = driver.page_source
             articleList = driver.find_elements(By.CLASS_NAME, "newsList_cont.clearfix")
 
             for n, article in enumerate(articleList):
                 title = article.find_element(By.CLASS_NAME, "headLine").text
-                text = article.find_element(By.CLASS_NAME, "list_txt").text
-                fname.write(title + "\n" + text +"\n\n")
-
-                soup = BeautifulSoup(text, 'html.parser')
-                list_txt_div = soup.find('div', class_='list_txt')
-                item1_text = list_txt_div.find('span').text
+                list_txt_div  = article.find_element(By.CSS_SELECTOR, ".list_txt")
+                span_text = list_txt_div.find_element(By.CSS_SELECTOR, "span").text
                 full_text = list_txt_div.text.strip()
+                articleText = full_text.replace(span_text, '').strip()
+                fname.write(title + "\n" + articleText +"\n\n")
 
 
     fname.close()
